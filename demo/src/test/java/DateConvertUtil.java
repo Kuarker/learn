2e07208cf6b7a39702b8da0981e6d792588892a9
lijiayan <kuarker.li@betterwood.com>
2021-10-11 11:44:00 +0800

    modify-时间转换工具类

--- demo/src/test/java/DateConvertUtil.java
+++ demo/src/test/java/DateConvertUtil.java
@@ -1,4 +1,5 @@
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -18,11 +19,28 @@ public class DateConvertUtil {
         return instant.atZone(zoneId).toLocalDateTime();
     }
 
+    public static LocalDateTime toLocalDateTime(LocalDate date) {
+
+        return toLocalDateTime(toDate(date));
+    }
+
+    public static LocalDate toLocalDate(Date date) {
+        Instant instant = date.toInstant();
+        ZoneId zoneId = ZoneId.systemDefault();
+        return instant.atZone(zoneId).toLocalDate();
+    }
+
     public static Date toDate(LocalDateTime localDateTime) {
 
         ZoneId zoneId = ZoneId.systemDefault();
         ZonedDateTime zdt = localDateTime.atZone(zoneId);
         return Date.from(zdt.toInstant());
+
+    }
+
+    public static Date toDate(LocalDate localDate) {
+        ZoneId zoneId = ZoneId.systemDefault();
+        return Date.from(localDate.atStartOfDay().atZone(zoneId).toInstant());
 
     }
 }
